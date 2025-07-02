949afb444241cade8c01f5e712ed6f2119f494f5
YasmimSophia <rodrigues.santos.yasmim@escola.pr.gov.br>
2025-07-02 21:26:38 +0000

    AULA-04

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener(" click", mostraPalavraChave);
 
 function mostraPalavraChave(){
-alert("Você clicou em mim ")
+const texto = document.querySelector("#resultado-palavrachave") .ariaValueMax;
+const campoResultado = document.querySelector("#resultado-palavrachave");
+const palavras = processaTexto(texto);
 
+campoResultado.textContent = palavras.join(",")
 }
+
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/);
+    return palavras;
+}
+
