c18fbba12f5c7375a92e16db3aaa0ac0e26fad41
YasmimSophia <rodrigues.santos.yasmim@escola.pr.gov.br>
2025-07-02 21:56:55 +0000

    AULA-06

--- script.js
+++ script.js
@@ -12,19 +12,26 @@ campoResultado.textContent = palavras.join(",")
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/);
-    
-     let frequencias = {} ;
-for(let i of palavras){
-    frequencias[i]= 0;
-    for (let j of palavras){
-        if (i==j){
-            frequencias[i]++;
-        }
-    }
-}
+    contaFrequencias(palavras);
+   const frequencia = contaFrequencias(palavras);
+   let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-console.log(grequencias);
+   function ordenaPalavra(p1,p2){
+    return frequencias[p2]- frequencias[p1];
+   }
    
-return palavras;
+    console.log(ordenadas);
+   return ordenadas.slice(0,15);
 }
 
+function contaFrequencias(palavras){}
+let frequencias = {} ;
+for(let i of palavras){
+frequencias[i]= 0;
+for (let j of palavras){
+   if (i==j){
+       frequencias[i]++;
+   }
+}
+}
+return frequencias;
